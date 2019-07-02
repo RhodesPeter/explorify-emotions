cf9554d0487a5700d3a8788471935503a410a3eb
Peter Rhodes <rhodespeter@hotmail.co.uk>
2019-07-02 14:20:50 +0100

    Add dot functionality

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const handleClick = (e) => {
   const emotion = e.target;
-  emotion.insertAdjacentHTML('afterbegin', '<span class="tick">✔︎</span>')
+
+  if (e.altKey === false) {
+    emotion.insertAdjacentHTML('afterbegin', '<span class="tick">✔︎</span>')
+  } else {
+    emotion.insertAdjacentHTML('afterbegin', '<div class="dot"></div>')
+  }
 };
 
 [...document.querySelectorAll('.emotions__emotion')]
   .forEach(el => el.addEventListener('click', handleClick));
-
